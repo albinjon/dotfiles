0f621de31e1f329d0d86af46b0fae4ae015ede5f
Albin Jonfelt <albin@abclabs.se>
2024-11-13 17:41:02 +0100

    updates

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,31 +1,39 @@
 return {
-  {
-    event = 'BufWinEnter',
-    'folke/noice.nvim',
-    dependencies = {
-      'rcarriga/nvim-notify',
-      opts = {
-        render = 'wrapped-compact',
-        stages = 'slide',
-        fps = 60,
-        timeout = 1800,
-        max_width = 80,
-      },
-    },
+  event = 'BufWinEnter',
+  'folke/noice.nvim',
+  dependencies = {
+    'rcarriga/nvim-notify',
     opts = {
-      lsp = {
-        hover = {
-          border = 'rounded',
-          silent = true,
+      render = 'wrapped-compact',
+      stages = 'slide',
+      fps = 60,
+      timeout = 1800,
+      max_width = 80,
+    },
+  },
+  config = function()
+    require('noice').setup({
+      routes = {
+        {
+          filter = {
+            event = 'notify',
+            find = 'No information available',
+          },
+          opts = {
+            skip = true,
+          },
         },
-        signature = {
-          border = 'rounded',
-          silent = true,
+      },
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
         },
       },
       presets = {
         lsp_doc_border = true,
       },
-    },
-  },
+    })
+  end,
 }
