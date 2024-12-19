b8631b0fc452e9be62f63e58cf0c214910c2c706
Albin Jonfelt <albin@abclabs.se>
2024-12-19 19:50:00 +0100

    update with variable, not working

--- dbee/.local/state/nvim/dbee/notes/global/orgs.sql
+++ dbee/.local/state/nvim/dbee/notes/global/orgs.sql
@@ -2,26 +2,42 @@ SELECT * FROM public."User"
 WHERE email LIKE 'albin@%.se'
 
 
-BEGIN;
--- Update User table
+BEGIN TRANSACTION;
+
+WITH vars AS (
+    SELECT 'albin@%.se' AS email_pattern
+)
+
 UPDATE "User" u
 SET deleted = true
-WHERE u.email = email AND u.deleted = false;
--- Update UserOrganization table
+WHERE u.email = ( SELECT emailQuery FROM constants ) AND u.deleted = false;
+
 UPDATE "UserOrganization" uo
 SET deleted = true
-WHERE uo.user_id = (SELECT id FROM "User" WHERE email = email)
+WHERE uo.user_id = (SELECT id FROM "User" WHERE email = ( SELECT emailQuery FROM constants ) )
   AND uo.deleted = false;
--- Update UserOrganizationRole table
+
 UPDATE "UserOrganizationRole" uor
 SET deleted = true
 WHERE uor.user_organization_id IN (
     SELECT uo.id 
     FROM "UserOrganization" uo
     JOIN "User" u ON u.id = uo.user_id
-    WHERE u.email = email
+    WHERE u.email = ( SELECT emailQuery FROM constants )
 )
 AND uor.deleted = false;
+
+UPDATE "UserOrganizationRoleNotification" uorn
+SET deleted = true
+WHERE uorn.user_organization_role_id IN (
+    SELECT uor.id
+    FROM "UserOrganizationRole" uor
+    JOIN "UserOrganization" uo ON uor.user_organization_id = uo.id
+    JOIN "User" u ON uo.user_id = u.id
+    WHERE u.email = ( SELECT emailQuery FROM constants )
+)
+AND uorn.deleted = false;
+
 SELECT DISTINCT
     u.email,
     u.name,
@@ -38,5 +54,6 @@ LEFT JOIN
 LEFT JOIN 
     "UserOrganizationRoleNotification" uorn ON uor.id = uorn.user_organization_role_id
 WHERE 
-    u.email = email;
-COMMIT; 
+    u.email = ( SELECT emailQuery FROM constants );
+
+COMMIT;
