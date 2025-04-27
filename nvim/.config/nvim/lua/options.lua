8cfaa740ddcb6734f70fd9fcad98328cee7de9f1
Albin Jonfelt <albin@abclabs.se>
2025-04-27 10:18:03 +0200

    update

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -10,10 +10,10 @@ vim.opt.ruler = false
 
 vim.opt.ttyfast = true
 
-vim.opt.expandtab = true -- expand tab input with spaces characters
+vim.opt.expandtab = true   -- expand tab input with spaces characters
 vim.opt.smartindent = true -- # syntax aware indentations for newline inserts
-vim.opt.tabstop = 2 -- # num of space characters per tab
-vim.opt.shiftwidth = 2 -- # spaces per indentation level
+vim.opt.tabstop = 2        -- # num of space characters per tab
+vim.opt.shiftwidth = 2     -- # spaces per indentation level
 
 vim.opt.conceallevel = 2
 
